e1af72aece0f3d52d3931c1f981ac9af287c1435
ojlee <dldhwn56@gmail.com>
2017-09-17 10:16:43 +0900

    drawable 추가

--- app/src/main/java/com/example/leeohju/becomeyoureyes/GuideActivity.java
+++ app/src/main/java/com/example/leeohju/becomeyoureyes/GuideActivity.java
@@ -9,6 +9,8 @@ public class GuideActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_guide);
+
+
     }
 }
